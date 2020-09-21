06b6466b92b3b3043b03dd6996829fae1773b489
Alex Garber <alexgarber6@gmail.com>
2020-09-21 15:00:33 -0600

    Documentation

---
+++ docs/index.md
@@ -0,0 +1,13 @@
+## Java
+
+* `edu`
+
+     *`deepdive`
+         
+          *`ArgConverter`
+          
+           A convberter that takes a single Celsius value from the command line
+          
+     
+
+## Flowgorithm
--- src/edu/cnm/deepdive/InputConverter.java
+++ src/edu/cnm/deepdive/InputConverter.java
@@ -1,14 +1,13 @@
 package edu.cnm.deepdive;
 
-import java.util.*;
-import java.lang.Math;
+import java.util.Scanner;
 
 public class InputConverter {
-    private static Scanner input = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         boolean running;
-        
+        Scanner input = new Scanner(System.in);
         running = true;
         while (running) {
             String userInput;
